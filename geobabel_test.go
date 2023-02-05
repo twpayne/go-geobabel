7129e4ac39c5bbdd1a8beea45295a3e6431057d6
Tom Payne <twpayne@gmail.com>
2023-02-05 21:57:33 +0100

    feat: add wkb conversions

--- geobabel_test.go
+++ geobabel_test.go
@@ -15,99 +15,101 @@ import (
 func TestAll(t *testing.T) {
 	geosContext := geos.NewContext()
 	for _, tc := range []struct {
-		name string
-		geom geom.T
-		geos *geos.Geom
-		orb  orb.Geometry
+		name        string
+		geomT       geom.T
+		geosGeom    *geos.Geom
+		orbGeometry orb.Geometry
+		skipWKB     string
 	}{
 		{
-			name: "Point",
-			geom: geom.NewPoint(geom.XY).MustSetCoords(geom.Coord{1, 2}),
-			geos: geosContext.NewPoint([]float64{1, 2}),
-			orb:  orb.Point{1, 2},
+			name:        "Point",
+			geomT:       geom.NewPoint(geom.XY).MustSetCoords(geom.Coord{1, 2}),
+			geosGeom:    geosContext.NewPoint([]float64{1, 2}),
+			orbGeometry: orb.Point{1, 2},
 		},
 		{
-			name: "LineString",
-			geom: geom.NewLineString(geom.XY).MustSetCoords([]geom.Coord{{1, 2}, {3, 4}}),
-			geos: geosContext.NewLineString([][]float64{{1, 2}, {3, 4}}),
-			orb:  orb.LineString{{1, 2}, {3, 4}},
+			name:        "LineString",
+			geomT:       geom.NewLineString(geom.XY).MustSetCoords([]geom.Coord{{1, 2}, {3, 4}}),
+			geosGeom:    geosContext.NewLineString([][]float64{{1, 2}, {3, 4}}),
+			orbGeometry: orb.LineString{{1, 2}, {3, 4}},
 		},
 		{
-			name: "LinearRing",
-			geom: geom.NewLinearRing(geom.XY).MustSetCoords([]geom.Coord{{1, 4}, {5, 2}, {3, 6}, {1, 4}}),
-			geos: geosContext.NewLinearRing([][]float64{{1, 4}, {5, 2}, {3, 6}, {1, 4}}),
-			orb:  orb.Ring{{1, 4}, {5, 2}, {3, 6}, {1, 4}},
+			name:        "LinearRing",
+			geomT:       geom.NewLinearRing(geom.XY).MustSetCoords([]geom.Coord{{1, 4}, {5, 2}, {3, 6}, {1, 4}}),
+			geosGeom:    geosContext.NewLinearRing([][]float64{{1, 4}, {5, 2}, {3, 6}, {1, 4}}),
+			orbGeometry: orb.Ring{{1, 4}, {5, 2}, {3, 6}, {1, 4}},
+			skipWKB:     "WKB does not support LinearRings",
 		},
 		{
 			name: "Polygon",
-			geom: geom.NewPolygon(geom.XY).MustSetCoords([][]geom.Coord{
+			geomT: geom.NewPolygon(geom.XY).MustSetCoords([][]geom.Coord{
 				{{0, 0}, {4, 0}, {4, 4}, {0, 0}},
 				{{2, 1}, {3, 1}, {3, 2}, {2, 1}},
 			}),
-			geos: geosContext.NewPolygon([][][]float64{
+			geosGeom: geosContext.NewPolygon([][][]float64{
 				{{0, 0}, {4, 0}, {4, 4}, {0, 0}},
 				{{2, 1}, {3, 1}, {3, 2}, {2, 1}},
 			}),
-			orb: orb.Polygon{
+			orbGeometry: orb.Polygon{
 				{{0, 0}, {4, 0}, {4, 4}, {0, 0}},
 				{{2, 1}, {3, 1}, {3, 2}, {2, 1}},
 			},
 		},
 		{
-			name: "MultiPoint",
-			geom: geom.NewMultiPoint(geom.XY).MustSetCoords([]geom.Coord{{1, 2}, {3, 4}}),
-			geos: geosContext.NewCollection(
+			name:  "MultiPoint",
+			geomT: geom.NewMultiPoint(geom.XY).MustSetCoords([]geom.Coord{{1, 2}, {3, 4}}),
+			geosGeom: geosContext.NewCollection(
 				geos.TypeIDMultiPoint,
 				[]*geos.Geom{
 					geosContext.NewPoint([]float64{1, 2}),
 					geosContext.NewPoint([]float64{3, 4}),
 				}),
-			orb: orb.MultiPoint{{1, 2}, {3, 4}},
+			orbGeometry: orb.MultiPoint{{1, 2}, {3, 4}},
 		},
 		{
 			name: "MultiLineString",
-			geom: geom.NewMultiLineString(geom.XY).MustSetCoords([][]geom.Coord{
+			geomT: geom.NewMultiLineString(geom.XY).MustSetCoords([][]geom.Coord{
 				{{1, 2}, {3, 4}},
 				{{5, 6}, {7, 8}},
 			}),
-			geos: geosContext.NewCollection(
+			geosGeom: geosContext.NewCollection(
 				geos.TypeIDMultiLineString,
 				[]*geos.Geom{
 					geosContext.NewLineString([][]float64{{1, 2}, {3, 4}}),
 					geosContext.NewLineString([][]float64{{5, 6}, {7, 8}}),
 				},
 			),
-			orb: orb.MultiLineString{
+			orbGeometry: orb.MultiLineString{
 				{{1, 2}, {3, 4}},
 				{{5, 6}, {7, 8}},
 			},
 		},
 		{
 			name: "MultiPolygon",
-			geom: geom.NewMultiPolygon(geom.XY).MustSetCoords([][][]geom.Coord{
+			geomT: geom.NewMultiPolygon(geom.XY).MustSetCoords([][][]geom.Coord{
 				{{{0, 0}, {1, 0}, {1, 1}, {0, 0}}},
 				{{{2, 1}, {3, 1}, {3, 2}, {2, 1}}},
 			}),
-			geos: geosContext.NewCollection(
+			geosGeom: geosContext.NewCollection(
 				geos.TypeIDMultiPolygon,
 				[]*geos.Geom{
 					geosContext.NewPolygon([][][]float64{{{0, 0}, {1, 0}, {1, 1}, {0, 0}}}),
 					geosContext.NewPolygon([][][]float64{{{2, 1}, {3, 1}, {3, 2}, {2, 1}}}),
 				},
 			),
-			orb: orb.MultiPolygon{
+			orbGeometry: orb.MultiPolygon{
 				{{{0, 0}, {1, 0}, {1, 1}, {0, 0}}},
 				{{{2, 1}, {3, 1}, {3, 2}, {2, 1}}},
 			},
 		},
 		{
 			name: "GeometryCollection",
-			geom: geom.NewGeometryCollection().MustPush(
+			geomT: geom.NewGeometryCollection().MustPush(
 				geom.NewPoint(geom.XY).MustSetCoords(geom.Coord{1, 2}),
 				geom.NewLineString(geom.XY).MustSetCoords([]geom.Coord{{1, 2}, {3, 4}}),
 				geom.NewPolygon(geom.XY).MustSetCoords([][]geom.Coord{{{0, 0}, {1, 0}, {1, 1}, {0, 0}}}),
 			),
-			geos: geosContext.NewCollection(
+			geosGeom: geosContext.NewCollection(
 				geos.TypeIDGeometryCollection,
 				[]*geos.Geom{
 					geosContext.NewPoint([]float64{1, 2}),
@@ -115,7 +117,7 @@ func TestAll(t *testing.T) {
 					geosContext.NewPolygon([][][]float64{{{0, 0}, {1, 0}, {1, 1}, {0, 0}}}),
 				},
 			),
-			orb: orb.Collection{
+			orbGeometry: orb.Collection{
 				orb.Point{1, 2},
 				orb.LineString{{1, 2}, {3, 4}},
 				orb.Polygon{{{0, 0}, {1, 0}, {1, 1}, {0, 0}}},
@@ -123,15 +125,58 @@ func TestAll(t *testing.T) {
 		},
 	} {
 		t.Run(tc.name, func(t *testing.T) {
-			require.True(t, tc.geos.IsValid())
-			require.Equal(t, "Valid Geometry", tc.geos.IsValidReason())
+			require.True(t, tc.geosGeom.IsValid())
+			require.Equal(t, "Valid Geometry", tc.geosGeom.IsValidReason())
 
-			assert.Equal(t, tc.geom, geobabel.NewGeomTFromOrbGeometry(tc.orb))
+			assert.Equal(t, tc.geomT, geobabel.NewGeomTFromOrbGeometry(tc.orbGeometry))
 
-			assert.True(t, tc.geos.Equals(geobabel.NewGEOSGeomFromOrbGeometry(geosContext, tc.orb)))
+			assert.True(t, tc.geosGeom.Equals(geobabel.NewGEOSGeomFromOrbGeometry(geosContext, tc.orbGeometry)))
 
-			assert.Equal(t, tc.orb, geobabel.NewOrbGeometryFromGEOSGeom(tc.geos))
-			assert.Equal(t, tc.orb, geobabel.NewOrbGeometryFromGeomT(tc.geom))
+			assert.Equal(t, tc.orbGeometry, geobabel.NewOrbGeometryFromGEOSGeom(tc.geosGeom))
+			assert.Equal(t, tc.orbGeometry, geobabel.NewOrbGeometryFromGeomT(tc.geomT))
+
+			if tc.skipWKB == "" {
+				geomWKB, err := geobabel.WKBFromGeomT(tc.geomT)
+				require.NoError(t, err)
+				geosWKB := geobabel.WKBFromGEOSGeom(tc.geosGeom)
+				orbWKB := geobabel.WKBFromOrbGeometry(tc.orbGeometry)
+
+				geomT, err := geobabel.NewGeomTFromWKB(geomWKB)
+				assert.NoError(t, err)
+				assert.Equal(t, tc.geomT, geomT)
+
+				geomT, err = geobabel.NewGeomTFromWKB(geosWKB)
+				assert.NoError(t, err)
+				assert.Equal(t, tc.geomT, geomT)
+
+				geomT, err = geobabel.NewGeomTFromWKB(orbWKB)
+				assert.NoError(t, err)
+				assert.Equal(t, tc.geomT, geomT)
+
+				geosGeom, err := geobabel.NewGEOSGeomFromWKB(geosContext, geomWKB)
+				assert.NoError(t, err)
+				assert.True(t, tc.geosGeom.Equals(geosGeom))
+
+				geosGeom, err = geobabel.NewGEOSGeomFromWKB(geosContext, geosWKB)
+				assert.NoError(t, err)
+				assert.True(t, tc.geosGeom.Equals(geosGeom))
+
+				geosGeom, err = geobabel.NewGEOSGeomFromWKB(geosContext, orbWKB)
+				assert.NoError(t, err)
+				assert.True(t, tc.geosGeom.Equals(geosGeom))
+
+				orbGeometry, err := geobabel.NewOrbGeometryFromWKB(geomWKB)
+				assert.NoError(t, err)
+				assert.Equal(t, tc.orbGeometry, orbGeometry)
+
+				orbGeometry, err = geobabel.NewOrbGeometryFromWKB(geosWKB)
+				assert.NoError(t, err)
+				assert.Equal(t, tc.orbGeometry, orbGeometry)
+
+				orbGeometry, err = geobabel.NewOrbGeometryFromWKB(orbWKB)
+				assert.NoError(t, err)
+				assert.Equal(t, tc.orbGeometry, orbGeometry)
+			}
 		})
 	}
 }
